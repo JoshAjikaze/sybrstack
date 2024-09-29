3575186206f7648b082bd77a604321acca95169d
Ajiwojuolorun Joshua Ayomide <ajiwojuolorunjoshua@gmail.com>
2024-09-29 19:04:25 +0100

    Latest Changes

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 var existingDiv = document.getElementById("copywright");
 existingDiv.innerHTML = `<p>© ${new Date().getFullYear()} SybrStack Technologies. All rights reserved. Various trademarks held by their respective owners.</p>`;
+
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+      navbar.classList.add('bg-[#272727]');
+    } else {
+      navbar.classList.remove('bg-[#272727]');   
+  
+    }
+  });
